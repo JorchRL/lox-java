93fbc246b06c83b6a11729b8cc64e77e43ea4fe4
Jorge Romero <jorch.rl@gmail.com>
2024-12-07 07:04:47 -0600

    Lexer and REPL implementation

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,8 +1,33 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class Scanner {
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or", TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
+    }
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
@@ -37,18 +62,10 @@ public class Scanner {
             case ';' : addToken(TokenType.SEMICOLON); break;
             case '*' : addToken(TokenType.STAR); break;
             // 2-character lexemes (i.e boolean operators)
-            case '!' :
-                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
-                break;
-            case '=' :
-                addToken(match('=') ? TokenType.EQUAL : TokenType.EQUAL_EQUAL);
-                break;
-            case '<' :
-                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
-                break;
-            case '>' :
-                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
-                break;
+            case '!' : addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+            case '=' : addToken(match('=') ? TokenType.EQUAL : TokenType.EQUAL_EQUAL); break;
+            case '<' : addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+            case '>' : addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
             case '/' :
                 if (match('/')) {
                     // the line is a comment.. ignore all characters in this line.
@@ -66,11 +83,18 @@ public class Scanner {
             case '\t':
                 break;
             // new line
-            case '\n' :
-                line ++;
-                break;
+            case '\n' : line ++; break;
+            // strings
+            case '"' : string(); break;
             default:
-                Lox.error(line, "Unexpected character");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                   identifier();
+                }
+                else {
+                    Lox.error(line, "Unexpected character");
+                }
                 break;
         }
     }
@@ -86,6 +110,58 @@ public class Scanner {
         return source.charAt(current);
     }
 
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    // matching operations
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line,"Unterminated String.");
+            return;
+        }
+        // we only reach here on the closing "
+        advance();
+        // cut up from the left pointer to the right pointer excluding the " "
+        String value = source.substring(start + 1, current - 1);
+        // and create a STRING token with its contents as value.
+        addToken(TokenType.STRING, value);
+    }
+
+    private void number(){
+        // check if the next chars are digits
+        while (isDigit(peek())) advance();
+
+        // if we peek a . we need to peek one extra character
+        if (peek() == '.' && isDigit(peekNext())){
+            advance(); //consume .
+            while(isDigit(peek())) advance();
+        }
+
+        // once we peek the next non-digit, we can end the lexing
+        addToken(TokenType.NUMBER,
+                Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+
+        // the idetifier is either a keyword or a symbol name
+        if (type == null) type = TokenType.IDENTIFIER;
+
+        addToken(type);
+    }
+
     private boolean match(char expected) {
         if (isAtEnd()) return false;
         if (source.charAt(current)!= expected) return false;
@@ -97,8 +173,23 @@ public class Scanner {
     }
 
     // utilities
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <+ '9';
+    }
+
+    private boolean isAlpha(char c ) {
+        return (c >= 'a' && c <= 'z' ||
+                c >= 'A' && c <= 'Z' ||
+                c == '_');
+    }
+
     private boolean isAtEnd() {
         return current >= source.length();
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     // Add tokens
